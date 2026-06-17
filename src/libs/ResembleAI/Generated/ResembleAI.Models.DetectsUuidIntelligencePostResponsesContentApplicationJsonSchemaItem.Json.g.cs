e4a125fa5a8bf0e0f0290fc12fcfbe3e883a6d39
Konstantin S. <havendv@gmail.com>
2026-06-17 15:09:15 +0400

    feat: Updated OpenAPI spec (#23)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ResembleAI/Generated/ResembleAI.Models.DetectsUuidIntelligencePostResponsesContentApplicationJsonSchemaItem.Json.g.cs
+++ src/libs/ResembleAI/Generated/ResembleAI.Models.DetectsUuidIntelligencePostResponsesContentApplicationJsonSchemaItem.Json.g.cs
@@ -17,6 +17,14 @@ namespace ResembleAI
         }
 
         /// <summary>
+        /// Serializes the current instance to a JSON string using the generated default JsonSerializerContext.
+        /// </summary>
+        public string ToJson()
+        {
+            return ToJson(global::ResembleAI.SourceGenerationContext.Default);
+        }
+
+        /// <summary>
         /// Serializes the current instance to a JSON string using the provided JsonSerializerOptions.
         /// </summary>
 #if NET8_0_OR_GREATER
@@ -26,6 +34,11 @@ namespace ResembleAI
         public string ToJson(
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
+            if (jsonSerializerOptions is null)
+            {
+                return ToJson(global::ResembleAI.SourceGenerationContext.Default);
+            }
+
             return global::System.Text.Json.JsonSerializer.Serialize(
                 this,
                 jsonSerializerOptions);
@@ -45,6 +58,17 @@ namespace ResembleAI
         }
 
         /// <summary>
+        /// Deserializes a JSON string using the generated default JsonSerializerContext.
+        /// </summary>
+        public static global::ResembleAI.DetectsUuidIntelligencePostResponsesContentApplicationJsonSchemaItem? FromJson(
+            string json)
+        {
+            return FromJson(
+                json,
+                global::ResembleAI.SourceGenerationContext.Default);
+        }
+
+        /// <summary>
         /// Deserializes a JSON string using the provided JsonSerializerOptions.
         /// </summary>
 #if NET8_0_OR_GREATER
@@ -55,6 +79,13 @@ namespace ResembleAI
             string json,
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
+            if (jsonSerializerOptions is null)
+            {
+                return FromJson(
+                    json,
+                    global::ResembleAI.SourceGenerationContext.Default);
+            }
+
             return global::System.Text.Json.JsonSerializer.Deserialize<global::ResembleAI.DetectsUuidIntelligencePostResponsesContentApplicationJsonSchemaItem>(
                 json,
                 jsonSerializerOptions);
@@ -74,6 +105,17 @@ namespace ResembleAI
         }
 
         /// <summary>
+        /// Deserializes a JSON stream using the generated default JsonSerializerContext.
+        /// </summary>
+        public static global::System.Threading.Tasks.ValueTask<global::ResembleAI.DetectsUuidIntelligencePostResponsesContentApplicationJsonSchemaItem?> FromJsonStreamAsync(
+            global::System.IO.Stream jsonStream)
+        {
+            return FromJsonStreamAsync(
+                jsonStream,
+                global::ResembleAI.SourceGenerationContext.Default);
+        }
+
+        /// <summary>
         /// Deserializes a JSON stream using the provided JsonSerializerOptions.
         /// </summary>
 #if NET8_0_OR_GREATER
@@ -84,6 +126,13 @@ namespace ResembleAI
             global::System.IO.Stream jsonStream,
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
+            if (jsonSerializerOptions is null)
+            {
+                return FromJsonStreamAsync(
+                    jsonStream,
+                    global::ResembleAI.SourceGenerationContext.Default);
+            }
+
             return global::System.Text.Json.JsonSerializer.DeserializeAsync<global::ResembleAI.DetectsUuidIntelligencePostResponsesContentApplicationJsonSchemaItem?>(
                 jsonStream,
                 jsonSerializerOptions);
